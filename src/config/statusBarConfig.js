532ab697d053e2cea94548ef1d1fe9258d71e042
Tauan Gabriel <tauan@Tauans-iMac.local>
2019-02-22 22:09:30 -0300

    Code do projeto React Native

---
+++ src/config/statusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('#3498db');
+StatusBar.setBarStyle('light-content');
---
+++ src/pages/product.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+const Product = () => (
+	<Text>Ola Mundo</Text>
+);
+export default Product;
